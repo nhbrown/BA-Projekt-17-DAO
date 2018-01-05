3be25143e47f0bd12a68ffa97fc547b5d8d8fed3
marcelrepe <marcel.repenning@gmail.com>
2018-01-05 12:52:25 +0100

    Erste Schritte mit Web3 und Aktualisierung der Contracts auf den neuesten Stand aus test-dev

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+ /**
+   * Applikation mit Web3 instanziieren nach dem Pet-Shop Tuorial
+   */
+App = {
+    web3Provider: null,
+    contracts: {},
+    /**
+     * Initialisiere Web3
+     */
+    init: function() {
+        return App.initWeb3();
+    },
+    /**
+     * Initialisiere Web3
+     */
+    initWeb3: function()
+    {
+      // Is there is an injected web3 instance?
+      if (typeof web3 !== 'undefined') {
+        App.web3Provider = web3.currentProvider;
+      } else {
+      // If no injected web3 instance is detected, fallback to the TestRPC
+        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      }
+        web3 = new Web3(App.web3Provider);
+  
+      return App.initContract();
+    },
+    /**
+     * Initialisiere Contracts
+     */
+    initContract: function() {
+    // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var CongressArtifact = data;
+      App.contracts.Congress = TruffleContract(CongressArtifact);
+
+    // Set the provider for our contract
+    App.contracts.Congress.setProvider(App.web3Provider);
+    },
+};
