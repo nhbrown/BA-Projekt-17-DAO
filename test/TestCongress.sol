427e7ec8fe8fab8efbde83da77cd98093c06844f
nhbrown <nicholas@hickson-brown.co.uk>
2018-01-08 01:56:22 +0100

    Added some more tests
    Added some more test cases and changed the routine so that this contract deploys a new Congress contract instead of using the one already deployed, so that the this contract is the owner and make calls to functions with the onlyOwner modifier.

--- test/TestCongress.sol
+++ test/TestCongress.sol
@@ -5,14 +5,20 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/Congress.sol";
 
 contract TestCongress {
-  Congress congress = Congress(DeployedAddresses.Congress());
+  Congress congress = new Congress(1, 5, 0);
 
   address memberOne = 0x123;
+  address memberTwo = 0x456;
+
   bool expectedTrue = true;
   bool expectedFalse = false;
 
   function testInitialMemberExists() public {
     Assert.equal(expectedTrue, congress.memberExists(0), "Member with address zero should exist.");
+  }
+
+  function testThisContractExistsAsMember() public {
+    Assert.equal(expectedTrue, congress.memberExists(this), "This contracts address should exist as a member.");
   }
 
   function testAddedMemberExists() public {
@@ -21,7 +27,25 @@ contract TestCongress {
     Assert.equal(expectedTrue, congress.memberExists(memberOne), "Member with the address 0x123 should exist.");
   }
 
-  function testGetContractAddress() public {
-    Assert.equal(congress, congress.getContractAddress(), "The function getContractAddress should return the proper address of the contract");
+  function testNotAddedMemberDoesNotExist() public {
+    Assert.equal(expectedFalse, congress.memberExists(memberTwo), "Member with the address 0x456 should not exist.");
+  }
+
+  function testRemovedMemberDoesNotExist() public {
+    congress.addMember(memberTwo);
+
+    Assert.equal(expectedTrue, congress.memberExists(memberTwo), "Member with the address 0x456 should exist.");
+
+    congress.removeMember(memberTwo);
+
+    Assert.equal(expectedFalse, congress.memberExists(memberTwo), "Member with the address 0x456 should not exist.");
+  }
+
+  function testGetProperAddressOfContract() public {
+    Assert.equal(congress, congress.getContractAddress(), "The proper address of the contract should be returned.");
+  }
+
+  function testOwnerOfCongressIsThisContract() public {
+    Assert.equal(this, congress.owner(), "This contract should be the owner of the contract.");
   }
 }
