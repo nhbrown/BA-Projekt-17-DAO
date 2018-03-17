83d48c7c6610223dc4c8b5927e4fde573efb5ff4
nhbrown <Nicholas@Nicholass-MBP.fritz.box>
2018-03-17 18:37:44 +0100

    Congress Cleanup
    Clean the Congress contract code up and added commentary.

--- contracts/Congress.sol
+++ contracts/Congress.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.16;
 
-contract owned {
+contract Owned {
     address public owner;
 
     function owned()  public {
@@ -17,70 +17,55 @@ contract owned {
     }
 }
 
-contract tokenRecipient {
-    event receivedEther(address sender, uint amount);
-    event receivedTokens(address _from, uint256 _value, address _token, bytes _extraData);
+contract Congress is Owned {
+    // Global variables of this contract
+    string public congressName;                     // user defined name of this contract
+    uint public minimumQuorum;                      // minimum amount of votes needed for a proposal to pass
+    uint public debatingPeriodInMinutes;            // time to vote on every proposal of this contract
+    uint public majorityMargin;                     // minimun amount of weighted votes in favour needed for a proposal to pass
+    Proposal[] public proposals;                    // all proposals of this contract
+    uint public numProposals;                       // amount of proposals this contract has
+    mapping (address => uint) public memberId;      // maps an ethereum address to an unsigned integer
+    Member[] public members;                        // all members of this contract
 
-    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public {
-        Token t = Token(_token);
-        require(t.transferFrom(_from, this, _value));
-        receivedTokens(_from, _value, _token, _extraData);
-    }
+    // Events of this contract 
+    event ProposalAdded(uint proposalID, string description);                                               // a proposal has been added
+    event Voted(uint proposalID, bool position, address voter);                                             // a vote has occured
+    event ProposalTallied(uint proposalID, uint result, uint quorum, bool active);                          // a proposal has been executed
+    event MembershipChanged(address member, bool isMember);                                                 // the membership status of an address has changed
+    event ChangeOfRules(uint newMinimumQuorum, uint newDebatingPeriodInMinutes, uint newMajorityMargin);    // the rules have been changed
+    event VotingWeightChanged(address member, uint newWeight);                                                 // the voting weight of a member has changed
 
-    function () payable  public {
-        receivedEther(msg.sender, msg.value);
-    }
-}
-
-interface Token {
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
-}
-
-contract Congress is owned, tokenRecipient {
-    // Contract Variables and events
-    string public congressName;
-    uint public minimumQuorum;
-    uint public debatingPeriodInMinutes;
-    uint public majorityMargin;
-    Proposal[] public proposals;
-    uint public numProposals;
-    mapping (address => uint) public memberId;
-    Member[] public members;
-
-    event ProposalAdded(uint proposalID, string description);
-    event Voted(uint proposalID, bool position, address voter);
-    event ProposalTallied(uint proposalID, uint result, uint quorum, bool active);
-    event MembershipChanged(address member, bool isMember);
-    event ChangeOfRules(uint newMinimumQuorum, uint newDebatingPeriodInMinutes, uint newMajorityMargin);
-
+    // A single proposal
     struct Proposal {
-        string description;
-        uint votingDeadline;
-        bool executed;
-        bool proposalPassed;
-        uint numberOfVotes;
-        uint inFavour;
-        uint opposedTo;
-        uint weightedInFavour;
-        uint weightedOpposedTo;
-        uint currentResult;
-        bytes32 proposalHash;
-        Vote[] votes;
-        mapping (address => bool) voted;
-        bool isValid; //set to true when data is added
+        string description;                 // the description of this proposal     
+        uint votingDeadline;                // the voting deadline for this proposal
+        bool executed;                      // wether or not this proposal has been executed
+        bool proposalPassed;                // wether or not this proposal passed
+        uint numberOfVotes;                 // the amount of votes on this proposal
+        uint inFavour;                      // the amount of votes in favour of this proposal
+        uint opposedTo;                     // the amount of votes opposed to this proposal
+        uint weightedInFavour;              // the accumulation of the weight of the votes in favour
+        uint weightedOpposedTo;             // the accumulation of the weight of the votes opposed to
+        uint currentResult;                 // the current result of this proposal
+        Vote[] votes;                       // all votes which occured on this proposal
+        mapping (address => bool) voted;    // maps an ethereum addres to a boolean
+        bool isValid;                       // wether or not this proposal has had data added to it
     }
 
+    // A single member
     struct Member {
-        address memberAddress; // address of this member
-        uint weight; // vote weight of this member
+        address memberAddress;              // address of this member
+        uint weight;                        // vote weight of this member
     }
 
+    // A single vote
     struct Vote {
-        bool inSupport;
-        address voter;
+        bool inSupport;                     // wether or not the vote was in favour
+        address voter;                      // the address of the member who commited the vote
     }
 
-    // Modifier that allows only members to vote and create new proposals
+    // Modifier that allows only members to execute a specific function.
     modifier onlyMembers {
         require(memberId[msg.sender] != 0);
         _;
@@ -88,22 +73,32 @@ contract Congress is owned, tokenRecipient {
 
     /**
      * Constructor function
+     *
+     * Creates a new instance of this contract.
+     *
+     * @param name The name of this Congress.
+     * @param weight The voting weight of the owner of this Congress.
+     * @param minimumQuorumForProposals The minimum amount of votes needed for a proposal to pass.
+     * @param minutesForDebate The amount of time in minutes to vote on every proposal of this contract.
+     * @param marginOfVotesForMajority The minimun amount of weighted votes in favour needed for a proposal to pass.
      */
     function Congress (string name, uint weight, uint minimumQuorumForProposals, uint minutesForDebate, uint marginOfVotesForMajority) payable public {
-        congressName = name;
+        congressName = name; 
+
         changeVotingRules(minimumQuorumForProposals, minutesForDebate, marginOfVotesForMajority);
-        // It’s necessary to add an empty first member
-        addMember(0, 0);
-        // and let's add the founder, to save a step later
+        
+        addMember(0, 0); // it’s necessary to add an empty first member
+
         addMember(owner, weight);
     }
 
     /**
      * Add member
      *
-     * Make `targetMember` a member.
+     * Make a specific address a member of this contract.
      *
-     * @param targetMember ethereum address to be added
+     * @param targetMember Ethereum address to be added.
+     * @param voteWeight The weight the vote of this member has.
      */
     function addMember(address targetMember, uint voteWeight) onlyOwner public {
         uint id = memberId[targetMember];
@@ -116,15 +111,15 @@ contract Congress is owned, tokenRecipient {
         members[id].memberAddress = targetMember;
         members[id].weight = voteWeight;
 
-        MembershipChanged(targetMember, true);
+        MembershipChanged(targetMember, false); // create a log of this event
     }
 
     /**
      * Remove member
      *
-     * @notice Remove membership from `targetMember`
+     * Remove the membership status of a specific address.
      *
-     * @param targetMember ethereum address to be removed
+     * @param targetMember Ethereum address to be removed.
      */
     function removeMember(address targetMember) onlyOwner public {
         require(memberId[targetMember] != 0);
@@ -135,49 +130,52 @@ contract Congress is owned, tokenRecipient {
 
         delete members[members.length-1];
         members.length--;
+
+        MembershipChanged(targetMember, true);
     }
 
     /**
+     * Change voting Weight
+     *
      * Changes the voting weight of the specified member.
      *
-     * @param targetMember ethereum address of the member which voting weight should be changed
-     * @param newWeight the new voting weight
+     * @param targetMember Ethereum address of the member whose voting weight should be changed.
+     * @param newWeight The new voting weight.
      */
     function changeVoteWeight(address targetMember, uint newWeight) onlyOwner public {
         require(memberId[targetMember] != 0);
 
         members[memberId[targetMember]].weight = newWeight;
+
+        VotingWeightChanged(targetMember, newWeight);
     }
 
     /**
      * Change voting rules
      *
-     * Make so that proposals need tobe discussed for at least `minutesForDebate/60` hours,
-     * have at least `minimumQuorumForProposals` votes, and have 50% + `marginOfVotesForMajority` votes to be executed
+     * Changes the voting rules of this contract.
      *
-     * @param minimumQuorumForProposals how many members must vote on a proposal for it to be executed
-     * @param minutesForDebate the minimum amount of delay between when a proposal is made and when it can be executed
-     * @param marginOfVotesForMajority the proposal needs to have 50% plus this number
+     * @param minimumQuorumForProposals How many members must vote on a proposal for it to pass.
+     * @param minutesForDebate The minimum amount of delay between when a proposal is made and when it can be executed.
+     * @param marginOfVotesForMajority What the end result of weighted votes has to be in order for a proposal to pass.
      */
     function changeVotingRules(uint minimumQuorumForProposals, uint minutesForDebate, uint marginOfVotesForMajority) onlyOwner public {
         minimumQuorum = minimumQuorumForProposals;
         debatingPeriodInMinutes = minutesForDebate;
         majorityMargin = marginOfVotesForMajority;
 
-        ChangeOfRules(minimumQuorum, debatingPeriodInMinutes, majorityMargin);
+        ChangeOfRules(minimumQuorum, debatingPeriodInMinutes, majorityMargin); // create a log of this event
     }
 
     /**
      * Add Proposal
      *
-     * Propose to send `weiAmount / 1e18` ether to `beneficiary` for `jobDescription`. `transactionBytecode ? Contains : Does not contain` code.
+     * Propose a new element of the BMC.
      *
-     * @param jobDescription Description of job
-     * @param transactionBytecode bytecode of transaction
+     * @param jobDescription Description of the specific element of the BMC.
      */
     function newProposal(
-        string jobDescription,
-        bytes transactionBytecode
+        string jobDescription
     )
         onlyOwner public
         returns (uint proposalID)
@@ -185,7 +183,6 @@ contract Congress is owned, tokenRecipient {
         proposalID = proposals.length++;
         Proposal storage p = proposals[proposalID];
         p.description = jobDescription;
-        p.proposalHash = keccak256(transactionBytecode);
         p.votingDeadline = now + debatingPeriodInMinutes * 1 minutes;
         p.executed = false;
         p.proposalPassed = false;
@@ -194,35 +191,18 @@ contract Congress is owned, tokenRecipient {
         p.opposedTo = 0;
         p.weightedInFavour = 0;
         p.weightedOpposedTo = 0;
-        ProposalAdded(proposalID, jobDescription);
+        ProposalAdded(proposalID, jobDescription); // create a log of this event
         numProposals = proposalID+1;
 
-        p.isValid = true; //data has been added
+        p.isValid = true; // data has been added
 
         return proposalID;
     }
-    
-    /**
-     * Check if a proposal code matches
-     *
-     * @param proposalNumber ID number of the proposal to query
-     * @param transactionBytecode bytecode of transaction
-     */
-    function checkProposalCode(
-        uint proposalNumber,
-        bytes transactionBytecode
-    )
-        constant public
-        returns (bool codeChecksOut)
-    {
-        Proposal storage p = proposals[proposalNumber];
-        return p.proposalHash == keccak256(transactionBytecode);
-    }
 
     /**
-     * Log a vote for a proposal
+     * Vote for a Proposal
      *
-     * Vote `supportsProposal? in support of : against` proposal #`proposalNumber`
+     * Logs a vote for a specific proposal.
      *
      * @param proposalNumber number of proposal
      * @param supportsProposal either in favor or against it
@@ -236,7 +216,7 @@ contract Congress is owned, tokenRecipient {
     {
         Proposal storage p = proposals[proposalNumber]; // Get the proposal
 
-        require(!p.voted[msg.sender]);                  // If has already voted, cancel
+        require(!p.voted[msg.sender]);                  // If this member has already voted, cancel
         p.voted[msg.sender] = true;                     // Set this voter as having voted
         p.numberOfVotes++;                              // Increase the number of votes
 
@@ -256,17 +236,16 @@ contract Congress is owned, tokenRecipient {
             }
         }
 
-        // Create a log of this event
-        Voted(proposalNumber, supportsProposal, msg.sender);
+        Voted(proposalNumber, supportsProposal, msg.sender); // create a log of this event
         return p.numberOfVotes;
     }
 
     /**
      * Finish vote
      *
-     * Count the votes proposal #`proposalNumber` and execute it if approved
+     * Check wether or not this proposal has passed and execute it, if all conditions are met.
      *
-     * @param proposalNumber proposal number
+     * @param proposalNumber The number of this proposal.
      */
     function executeProposal(uint proposalNumber) onlyOwner payable public {
         Proposal storage p = proposals[proposalNumber];
@@ -275,9 +254,9 @@ contract Congress is owned, tokenRecipient {
         require(now >= p.votingDeadline);
         require(!p.executed);                                  
 
-        // ...then execute result
+        // then execute this proposal
         if (p.currentResult >= majorityMargin && p.numberOfVotes >= minimumQuorum) {
-            // Proposal passed; execute the transaction
+            // Proposal passed
             p.executed = true;
             p.proposalPassed = true;
         } else {
@@ -291,9 +270,12 @@ contract Congress is owned, tokenRecipient {
     }
 
     /**
-     * Check wether or not a given address exists as a member of this contract.
+     * Member exists
+     * 
+     * Check wether or not a given address is a member of this contract.
      * 
      * @param targetMember The address to be checked for member status.
+     *
      * @return A boolean representing wether or not the given address is a member of this contract.
      */
     function memberExists(address targetMember) external view returns (bool) {
@@ -307,9 +289,12 @@ contract Congress is owned, tokenRecipient {
     }
 
     /**
+     * Proposal exists
+     *
      * Check wether or not a proposal corresponding to the given proposalID exists.
      *
      * @param proposalID The ID of the proposal to be checked.
+     * 
      * @return A boolean representing wether or not a proposal with the given ID was created.
      */
     function proposalExists(uint proposalID) external view returns (bool) {
@@ -321,26 +306,29 @@ contract Congress is owned, tokenRecipient {
     }
 
     /**
-     * Check wether or not a member has successfully voted on a specific proposal.
-     *
-     * @dev This method really only exists for testing purposes. 
-            Can probably be disabled for production.
-     *
-     * @param targetMember The address of the member to be checked.
-     * @param proposalID The ID of the proposal to be checked.
-     * @return A boolean representing wether or not the specified member has successfully voted.
-     */
-    function memberHasVoted(address targetMember, uint proposalID) external view returns (bool) {
-        return proposals[proposalID].voted[targetMember];
-    }
-
-    /**
+     * Get proposal description
+     * 
      * Return the description of a given proposal.
      *
      * @param proposalID The ID of the proposal.
+     *
      * @return A string representing the description of the proposal.
      */
     function getProposalDescription(uint proposalID) public constant returns (string) {
         return proposals[proposalID].description;
     }
+
+    /**
+     * Check wether or not a member has successfully voted on a specific proposal.
+     *
+     * @dev This method really only exists for testing purposes. 
+     *      Can probably be deprecated for production.
+     *
+     * @param targetMember The address of the member to be checked.
+     * @param proposalID The ID of the proposal to be checked.
+     * @return A boolean representing wether or not the specified member has successfully voted.
+     */
+    //function memberHasVoted(address targetMember, uint proposalID) external view returns (bool) {
+    //    return proposals[proposalID].voted[targetMember];
+    //}
 }
